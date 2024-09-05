c43ac89e7b9df6de833cf1e3538020071788c15b
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 23:40:02 +0600

    logged-in user info from the session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -6,20 +6,25 @@ const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
   console.log(session);
   return (
-    <div>
-      <h1 className="text-4xl text-center mt-10">
-        Welcome {session?.user?.name}
-      </h1>
-      <h2 className="text-2xl text-center mt-10">
-        Logged in using: {session?.user?.email}
-      </h2>
-      <Image
-        src={session?.user?.image as string}
-        alt={`Image of ${session?.user?.name}`}
-        width={300}
-        height={400}
-      />
-    </div>
+    <>
+      {session?.user && (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h2 className="text-2xl text-center mt-10">
+            Logged in using: {session?.user?.email}
+          </h2>
+          <Image
+            src={session?.user?.image as string}
+            alt={`Image of ${session?.user?.name}`}
+            width={300}
+            height={400}
+            className="mx-auto rounded-full"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
