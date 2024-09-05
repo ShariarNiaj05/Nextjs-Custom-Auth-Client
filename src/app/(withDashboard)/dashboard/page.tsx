a03d54f0b0a8fb7f52425dcc565f6d92a4f34d41
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 23:35:16 +0600

    logged-in user info from the session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -9,6 +9,9 @@ const DashboardPage = async () => {
       <h1 className="text-4xl text-center mt-10">
         Welcome {session?.user?.name}
       </h1>
+      <h2 className="text-2xl text-center mt-10">
+        Logged in using: {session?.user?.email}
+      </h2>
     </div>
   );
 };
