dea8952c3be6cc6e7e0164f00dd4e14fe433d5ec
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 01:20:05 +0600

    Login user using Next.js server actions and a custom server

--- src/utils/actions/loginUser.ts
+++ src/utils/actions/loginUser.ts
@@ -3,7 +3,7 @@
 import { UserData } from "@/app/register/page";
 
 export const loginUser = async (data: Partial<UserData>) => {
-  const res = await fetch(`${process.env.BACKEND_URL}/register`, {
+  const res = await fetch(`${process.env.BACKEND_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
