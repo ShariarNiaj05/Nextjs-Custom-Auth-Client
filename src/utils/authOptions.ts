281ba80c334d8a73ed75dfde87eab402459d1017
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 21:51:53 +0600

    GitHub authentication system

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,13 +1,12 @@
 import { NextAuthOptions } from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
-    /*  GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here */
   ],
 };
