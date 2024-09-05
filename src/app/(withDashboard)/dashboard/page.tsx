67bb97c575fde2c0c59e5e9f5f0a8d84392d29d6
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-05 23:37:41 +0600

    logged-in user info from the session

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
@@ -12,6 +13,12 @@ const DashboardPage = async () => {
       <h2 className="text-2xl text-center mt-10">
         Logged in using: {session?.user?.email}
       </h2>
+      <Image
+        src={session?.user?.image as string}
+        alt={`Image of ${session?.user?.name}`}
+        width={300}
+        height={400}
+      />
     </div>
   );
 };
