b23411ffc82827c03f00b2b1e7907d22d5270883
ShariarNiaj05 <shariarn85@gmail.com>
2024-09-06 01:00:14 +0600

    Register user using Next.js server actions and a custom server

--- src/utils/actions/registerUser.ts
+++ src/utils/actions/registerUser.ts
@@ -1,6 +1,14 @@
 "use server";
 
-export const registerUser = async () => {
-  const res = await fetch(`${process.env.BACKEND_URL}/register`);
-  return;
+export const registerUser = async (data) => {
+  const res = await fetch(`${process.env.BACKEND_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    cache: "no-store",
+  });
+  const userInfo = await res.json();
+  return userInfo;
 };
